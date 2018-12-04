85c2fae8696d23187ee185cfc866e565298129b2
Harrison <johnpwease@yahoo.com>
2018-12-04 15:00:10 -0800

    toString Update
    Updating toString and adding a constant delimeter.

--- TCSS 360 Weiss Beans/src/model/Project.java
+++ TCSS 360 Weiss Beans/src/model/Project.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 
 public class Project {
 	
+	// Constant for splitting at ":"
 	private static final String DELIMETER = ":";
 	
 	//instance fields, everything that will be needed for user to add to a diy project.
